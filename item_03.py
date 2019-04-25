2df80034f1bdecbaae79ed4a98d08aff243e3e19
Joon-S <niceweather94@gmail.com>
2019-04-25 13:40:18 +0900

    first init

---
+++ item_03.py
@@ -0,0 +1,15 @@
+# str, byes 를 입력받고 str 변환
+def to_str(bytes_or_str):
+    if isinstance(bytes_or_str, bytes):
+        value = bytes_or_str.decode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
+
+
+def to_bytes(bytes_or_str):
+    if isinstance(bytes_or_str, str):
+        value = bytes_or_str.encode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
